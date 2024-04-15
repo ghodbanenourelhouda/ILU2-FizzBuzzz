b3e7f75ffd69d1ad439d704b87c75e466f30159e
ghodbanenourelhouda <gho.nourelhouda@gmail.com>
2024-04-15 21:06:46 +0200

    Fizzbuzz (2) retourne 2

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,6 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	public static Object fizzbuzz (int i) {
-		return "1" ;
+		return Integer.toString (i) ;
 	}
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
 	@Test
-	void test() {
-		assertEquals(FizzBuzz.fizzbuzz(1), "1");
+	void test_un() {
+		assertEquals(FizzBuzz.fizzbuzz(1),"1");
 	}
-
+	
+	@Test
+	void test_deux() {
+		assertEquals(FizzBuzz.fizzbuzz(2), "2");
+	}
 }
